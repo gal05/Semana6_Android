465d9464ffcac310227dde37f59911d1287cf8c8
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-04-03 09:33:19 -0500

    GG

--- app/src/main/java/pe/edu/guerra/semana6/MainActivity.java
+++ app/src/main/java/pe/edu/guerra/semana6/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.PermissionRequest;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     private EditText phoneNumberEditText;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED)
             startActivity(intent);
     }
+
 
 
 
